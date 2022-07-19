ea9830ebb15b86416dbf53448b1cbaf97c122fdc
JakubWo <tosiekandjojo@gmail.com>
2022-07-19 23:19:19 +0200

    configuration commit for localhost development

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-%zb3+h_5sh#=4tm9^yiru&j^*w7p-qm(o5u(w@nsp!91*wbag=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['158.101.178.194']
+ALLOWED_HOSTS = [
+    '158.101.178.194',
+    'weather-station.localhost'
+]
 
 
 # Application definition
