be6cfef4f631215a69a9e85c5f88da72c2c95e67
JakubWo <tosiekandjojo@gmail.com>
2022-12-02 00:12:40 +0100

    fix: code cleanup add: change models to take new API format add: replace list of dicts to list of Plot objects

--- website/main/models.py
+++ website/main/models.py
@@ -1,36 +1,13 @@
 import random
 
-# vok - lotne związki organiczne
 
-"""
-[
-  {
-    "timestamp": 1658249939.278797,
-    "temperature": 26.99,
-    "humidity": 42.77,
-    "pressure": 1016.099976,
-    "voc": 19884
-  },
-  {
-    "timestamp": 1658249894.8346677,
-    "temperature": 26.98,
-    "humidity": 42.384998,
-    "pressure": 1016.099976,
-    "voc": 20036
-  },
-  {
-    "timestamp": 1658249872.3584795,
-    "temperature": 26.98,
-    "humidity": 42.183998,
-    "pressure": 1016.099976,
-    "voc": 19959
-  }
-]
-"""
+def map_data_to_object(measurements: dict) -> list:
+    plots = list()
 
+    for key in measurements:
+        plots.append(Plot(key, measurements[key]))
 
-def random_color() -> str:
-    return '#%02X%02X%02X' % (random.randint(0, 200), random.randint(0, 200), random.randint(0, 200))
+    return plots
 
 
 def map_known_keys(key: str) -> str:
@@ -44,90 +21,26 @@ def map_known_keys(key: str) -> str:
     return switch.get(key, key)
 
 
-def load_single_chart_data(records: list, plot_name: str) -> dict:
-    plots = dict()
-    plot_records = dict()
-
-    for record in records:
-        if type(record) is not dict or 'timestamp' not in record.keys():
-            continue
-
-        timestamp = record['timestamp']
-
-        for key in record:
-            if key == 'timestamp':
-                continue
-
-            elif key not in plots:
-                plots[key] = Plot(key, map_known_keys(key), random_color())
-
-            elif key == plot_name:
-                plots[key].add_record(timestamp, record[key])
-
-            elif key not in plot_records:
-                plot_records[key] = 1
-
-            else:
-                plot_records[key] += 1
-
-    # Don't show plots with small amount of data
-    # Allow 0 for generating sidebar views
-    for key in plot_records:
-        if plot_records[key] < 50:
-            plots.pop(plot_name)
-
-    return plots
-
-
-def load_charts_data(records: list) -> dict:
-    plots = dict()
-
-    for record in records:
-        if type(record) is not dict or 'timestamp' not in record.keys():
-            continue
-
-        timestamp = record['timestamp']
-
-        for key in record:
-            if key == 'timestamp':
-                continue
-            elif key not in plots:
-                plots[key] = Plot(key, map_known_keys(key), random_color())
-
-            plots[key].add_record(timestamp, record[key])
-
-    # Don't show plots with small amount of data
-    # Allow 0 for generating sidebar views
-    for key in plots:
-        if plots[key].count_records() < 50:
-            plots.pop(key)
-
-    return plots
+def random_color() -> str:
+    return '#%02X%02X%02X' % (random.randint(0, 200), random.randint(0, 200), random.randint(0, 200))
 
 
 class Plot:
-    def __init__(self, name: str, mapped_name: str, color: str) -> None:
+    def __init__(self, name: str, records: list):
         self.__name = name
-        self.__mapped_name = mapped_name
-        self.__color = color
-        self.__timestamps = list()
-        self.__values = list()
+        self.__mapped_name = map_known_keys(name)
+        self.__records = records
+        self.__color = random_color()
 
-    def add_record(self, timestamp: float, value) -> None:
-        if value is None:
-            return
+    def get_name(self):
+        return self.__name
 
-        self.__timestamps.append(int(timestamp * 1000))
-        self.__values.append(value)
+    def get_mapped_name(self):
+        return self.__mapped_name
 
-    def get_records(self) -> dict:
-        return {
-            'name': self.__name,
-            'mapped_name': self.__mapped_name,
-            'timestamps': self.__timestamps,
-            'values': self.__values,
-            'color': self.__color
-        }
+    def get_records(self):
+        return self.__records
 
-    def count_records(self) -> int:
-        return len(self.__timestamps)
+    def get_color(self):
+        return self.__color
+
--- website/main/views.py
+++ website/main/views.py
@@ -1,19 +1,17 @@
 import json
 
-from django.http import HttpResponse, JsonResponse
-# from django.template.library im
 from django.template.defaultfilters import register
 from django.views.generic import TemplateView
 from requests import get
 from typing import Dict
 
-from .models import load_charts_data, load_single_chart_data
+from .models import map_data_to_object
 
 
-@register.filter(name='get_list_elem')
-def get_list_elem(value, arg):
+@register.filter(name='get_plot')
+def get_plot(value: list, arg: str):
     for plot in value:
-        if plot['name'] == arg:
+        if plot.get_name() == arg:
             return plot
 
     return None
@@ -24,12 +22,10 @@ class MainView(TemplateView):
 
     def get_context_data(self, **kwargs) -> Dict[str, any]:
         context = super().get_context_data(**kwargs)
-        data = get('https://144.24.161.226/getMeasurements?format=json&last=100', verify=False)
-        plots = load_charts_data(json.loads(data.text))
-        context['plots'] = list()
 
-        for key in plots:
-            context['plots'].append(plots[key].get_records())
+        data = get('https://144.24.161.226/getMeasurements?format=json&last=100', verify=False)
+
+        context['plots'] = map_data_to_object(json.loads(data.text))
 
         return context
 
@@ -37,14 +33,11 @@ class MainView(TemplateView):
 class PlotView(TemplateView):
     template_name = 'plot/index.html'
 
-    # plot_name = variable
     def get_context_data(self, **kwargs) -> Dict[str, any]:
         context = super().get_context_data(**kwargs)
-        data = get('https://144.24.161.226/getMeasurements?format=json', verify=False)
-        plots = load_single_chart_data(json.loads(data.text), context['plot_name'])
-        context['plots'] = list()
 
-        for key in plots:
-            context['plots'].append(plots[key].get_records())
+        data = get('https://144.24.161.226/getMeasurements?format=json', verify=False)
+
+        context['plots'] = map_data_to_object(json.loads(data.text))
 
         return context
