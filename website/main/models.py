d5a260c4513191546943b524e9f285ed531991af
JakubWo <tosiekandjojo@gmail.com>
2022-12-03 15:30:15 +0100

    add: tables with basic plot data summarized add: helper templates for tables add: template ext function for getting new plot object for given period add: code cleanup add: data summary functions for plot object

--- website/main/models.py
+++ website/main/models.py
@@ -1,11 +1,11 @@
 import random
 
 
-def map_data_to_object(measurements: dict) -> list:
+def map_data_to_object(measurements: dict, period: str) -> list:
     plots = list()
 
     for key in measurements:
-        plots.append(Plot(key, measurements[key]))
+        plots.append(Plot(key, measurements[key], period))
 
     return plots
 
@@ -26,21 +26,57 @@ def random_color() -> str:
 
 
 class Plot:
-    def __init__(self, name: str, records: list):
+    def __init__(self, name: str, records: list, period: str):
         self.__name = name
         self.__mapped_name = map_known_keys(name)
-        self.__records = records
         self.__color = random_color()
+        self.__records = records
+        self.__period = period
 
-    def get_name(self):
+        if period != 'cały okres' and period != 'ostatnie 100':
+            self.__records_length = len(records)
+            self.__time_gaps = self.get_time_gaps()
+            self.__time_gaps_length = len(self.__time_gaps)
+
+    def get_time_gaps(self) -> list:
+        time_gaps = list()
+        for i in range(1, self.__records_length):
+            time_gaps.append(self.__records[i-1]['timestamp'] - self.__records[i]['timestamp'])
+
+        return time_gaps
+
+    def get_name(self) -> str:
         return self.__name
 
-    def get_mapped_name(self):
+    def get_mapped_name(self) -> str:
         return self.__mapped_name
 
-    def get_records(self):
-        return self.__records
-
-    def get_color(self):
+    def get_color(self) -> str:
         return self.__color
 
+    def get_records(self) -> list:
+        return self.__records
+
+    def get_period(self) -> str:
+        return self.__period
+
+    def get_records_length(self) -> int:
+        return self.__records_length
+
+    def get_avg_value(self) -> float:
+        return sum(record['value'] for record in self.__records) / self.__records_length
+
+    def get_min_value(self) -> float:
+        return min(record['value'] for record in self.__records)
+
+    def get_max_value(self) -> float:
+        return max(record['value'] for record in self.__records)
+
+    def get_avg_period_between_records(self) -> float:
+        return sum(time_gap for time_gap in self.__time_gaps) / len(self.__time_gaps)
+
+    def get_min_period_between_records(self) -> int:
+        return min(self.__time_gaps)
+
+    def get_max_period_between_records(self) -> int:
+        return max(self.__time_gaps)
--- website/main/views.py
+++ website/main/views.py
@@ -4,17 +4,44 @@ from django.template.defaultfilters import register
 from django.views.generic import TemplateView
 from requests import get
 from typing import Dict
+import time
 
-from .models import map_data_to_object
+from .models import map_data_to_object, Plot
 
 
 @register.filter(name='get_plot')
-def get_plot(value: list, arg: str):
+def get_plot_by_name(value: list, arg: str):
     for plot in value:
         if plot.get_name() == arg:
             return plot
 
     return None
+
+
+@register.filter(name='get_last_week_plot')
+def get_last_week_plot(value: Plot):
+    records = list()
+    timestamp = int((time.time() - 604800) * 1000)
+
+    for record in value.get_records():
+        if record['timestamp'] < timestamp:
+            break
+        records.append(record)
+
+    return Plot(value.get_name(), records, 'ostatni tydzień')
+
+
+@register.filter(name='get_last_month_plot')
+def get_last_month_plot(value: Plot):
+    records = list()
+    timestamp = int((time.time() - 2629743) * 1000)
+
+    for record in value.get_records():
+        if record['timestamp'] < timestamp:
+            break
+        records.append(record)
+
+    return Plot(value.get_name(), records, 'ostatni miesiąc')
 
 
 class MainView(TemplateView):
@@ -25,7 +52,7 @@ class MainView(TemplateView):
 
         data = get('https://144.24.161.226/getMeasurements?format=json&last=100', verify=False)
 
-        context['plots'] = map_data_to_object(json.loads(data.text))
+        context['plots'] = map_data_to_object(json.loads(data.text), 'ostatnie 100')
 
         return context
 
@@ -38,6 +65,6 @@ class PlotView(TemplateView):
 
         data = get('https://144.24.161.226/getMeasurements?format=json&measurement_type='+context['plot_name'], verify=False)
 
-        context['plots'] = map_data_to_object(json.loads(data.text))
+        context['plots'] = map_data_to_object(json.loads(data.text), 'cały okres')
 
         return context
