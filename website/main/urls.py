8aa55b98d0208e4105d97f08c3644ffe68bb7d03
JakubWo <tosiekandjojo@gmail.com>
2023-01-15 00:00:49 +0100

    add: site titles add: data archive add: csv downloading

--- website/main/urls.py
+++ website/main/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from .views import MainView
+from .views import ArchiveView
 from .views import PlotView
 from .views import get_last_measurement
+from .views import some_view
 
 urlpatterns = [
     path('', MainView.as_view(), name='index'),
+    path('archive', ArchiveView.as_view(), name='archive'),
     path('plot/<str:plot_name>', PlotView.as_view(), name='plot'),
-    path('ajax/last-measurement', get_last_measurement, name='last_measurement')
+    path('ajax/last-measurement', get_last_measurement, name='last_measurement'),
+    path('ajax/download/<str:plot_name>', some_view, name='some_view')
 ]
--- website/main/views.py
+++ website/main/views.py
@@ -1,11 +1,14 @@
+import csv
 import json
+import time
 
 from django.http import JsonResponse
+from django.http import HttpResponse
 from django.template.defaultfilters import register
 from django.views.generic import TemplateView
 from requests import get
 from typing import Dict
-import time
+from datetime import datetime
 
 from .models import map_data_to_object, Plot
 
@@ -52,6 +55,60 @@ def get_last_measurement(request) -> JsonResponse:
     data = get('https://144.24.161.226/getMeasurements?format=json&last=1', verify=False)
 
     return JsonResponse(json.loads(data.text))
+
+
+def some_view(request, plot_name):
+    filename = 'weather-station_' + plot_name + '_' + str(datetime.now().strftime('%d-%m-%Y_%H-%M-%S')) + '.csv'
+    response = HttpResponse(
+        content_type='text/csv',
+        headers={'Content-Disposition': 'attachment; filename="' + filename + '"'},
+    )
+
+    url_params = ''
+    if plot_name != 'all_data':
+        url_params = '&measurement_type=' + plot_name
+
+    data = get('https://144.24.161.226/getMeasurements?format=json' + url_params, verify=False)
+    data = json.loads(data.text)
+    writer = csv.writer(response)
+
+    if plot_name == 'all_data':
+        csv_data = dict()
+        title_row = ['timestamp']
+        for key in data:
+            title_row.append(key)
+            for record in data[key]:
+                if record['timestamp'] not in csv_data:
+                    csv_data[record['timestamp']] = []
+                    csv_data[record['timestamp']].append(record['timestamp'])
+
+                csv_data[record['timestamp']].append(record['value'])
+
+        writer.writerow(title_row)
+
+        for key in csv_data:
+            writer.writerow(csv_data[key])
+
+    else:
+        writer.writerow(['timestamp', plot_name])
+        for record in data[plot_name]:
+            writer.writerow([record['timestamp'], record['value']])
+
+    return response
+
+
+class ArchiveView(TemplateView):
+    template_name = 'archive/index.html'
+
+    def get_context_data(self, **kwargs) -> Dict[str, any]:
+        context = super().get_context_data(**kwargs)
+
+        data = get('https://144.24.161.226/getMeasurements?format=json&measurement_type=',
+                   verify=False)
+
+        context['plots'] = map_data_to_object(json.loads(data.text), 'limit')
+
+        return context
 
 
 class MainView(TemplateView):
